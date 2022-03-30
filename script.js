d20218d24779b7ecf98a7e0382847ea7e1ad71c4
Julio Reis <juliokreis@hotmail.com>
2022-03-30 00:49:16 -0300

    input alterado para span

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let n1 = document.querySelector('#n1')
 let n2 = document.querySelector('#n2')
-let resultado = document.querySelector('span')
+let resultado = document.querySelector('p')
 
 function somar() {
     resultado.innerHTML = parseInt(n1.value) + parseInt(n2.value)
